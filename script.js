1060852cc35b3f683cd506c0af82186d6ca72f59
ingalgi_krishna <krishnaingalgi1632@gmail.com>
2024-11-26 17:57:10 +0530

    indianexpress

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("subscribe").addEventListener("click", function () {
+    alert("Thank you for subscribing!");
+});
+
+document.getElementById("sign-in").addEventListener("click", function () {
+    alert("Redirecting to Sign-In page...");
+});
